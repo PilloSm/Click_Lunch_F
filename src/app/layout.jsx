a2d8d70d4830dcff3823dbd8baf9f4af546b5c51
nietomar <nietomartinezpedronestor@gmail.com>
2023-11-27 11:13:16 -0600

    ajs

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -6,6 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'ClickLunch',
   description: 'artick monkeys god y el tercer album de mcr es god (no leo lloros)',
+  icon: 'https://i.ibb.co/nBySv9Y/logo-nobg.png'
 }
 
 export default function RootLayout({ children }) {
