01f4c09cfb9834ab10e28c7da4bfb94e134e1553
PilloSm <corzo.arguello.luis@gmail.com>
2023-11-28 10:52:17 -0600

    3

--- src/app/client/pedidos/[id]/page.jsx
+++ src/app/client/pedidos/[id]/page.jsx
@@ -2,5 +2,11 @@ import TablaPedidos from "@/components/tablaPedidos";
 import ValUsu from "@/components/validarUsu";
 
 export default function ({ params }) {
-  return <TablaPedidos id_cuenta={params.id} />;
+  return (
+    <>
+      <ValUsu>
+        <TablaPedidos id_cuenta={params.id} />;
+      </ValUsu>
+    </>
+  );
 }
