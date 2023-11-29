a3ae2f2bf3eeecfd8743082c2e30acc659ac7ca0
PilloSm <corzo.arguello.luis@gmail.com>
2023-11-29 10:04:49 -0600

    captcha

--- src/libs/db.js
+++ src/libs/db.js
@@ -1,20 +1,20 @@
 import { createPool } from "mysql2/promise";
 
-// export const conn = createPool({
-//   host: "localhost",
-//   user: "root",
-//   password: "LuMITY_BV1",
-//   port: 3308,
-//   database: "clickluch",
-// });
-
 export const conn = createPool({
-    host:'localhost',
-    user:'root',
-    password:'LuMITY_BV1',
-    port:3306,
-    database:'clickluch'
-})
+  host: "localhost",
+  user: "root",
+  password: "n0m3l0",
+  port: 3308,
+  database: "clickluch",
+});
+
+// export const conn = createPool({
+//     host:'localhost',
+//     user:'root',
+//     password:'LuMITY_BV1',
+//     port:3306,
+//     database:'clickluch'
+// })
 
 // database: clicklunch
 // username: 2l3m81j21fxzbehsiz9t
