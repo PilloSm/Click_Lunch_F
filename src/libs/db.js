abcd4714986b42732d0faba5f36a01fd9dc213ad
PilloSm <corzo.arguello.luis@gmail.com>
2023-11-30 00:54:08 -0600

    dasda

--- src/libs/db.js
+++ src/libs/db.js
@@ -1,11 +1,10 @@
 import { createPool } from "mysql2/promise";
 
 export const conn = createPool({
-  database: "railway",
+  database: "clicklunch",
   user: process.env.DATABASE_USERNAME,
   host: process.env.DATABASE_HOST,
   password: process.env.DATABASE_PASSWORD,
-  port: 57256,
   ssl: {
     rejectUnauthorized: false,
   },
