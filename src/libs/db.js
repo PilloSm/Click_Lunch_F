07eb30097ea5c43d067a12e0fe4ae8f84cf1d477
PilloSm <corzo.arguello.luis@gmail.com>
2023-11-30 00:03:17 -0600

    como puede ser

--- src/libs/db.js
+++ src/libs/db.js
@@ -1,7 +1,7 @@
 import { createPool } from "mysql2/promise";
 
 export const conn = createPool({
-  ddatabase: 'clicklunch',
+  database: 'clicklunch',
   user: 'vo9zbl973a7jeoax56hs',
   host: 'aws.connect.psdb.cloud',
   password: 'pscale_pw_G7mXmMTKOmKxWsfU7IdVoiPJKBAaIwcqbU96wgrEKD8',
