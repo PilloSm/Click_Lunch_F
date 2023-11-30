be1803d2cdc3ec462fb729ec31e45b03028a1daf
nietomar <nietomartinezpedronestor@gmail.com>
2023-11-30 03:04:39 -0600

    l

--- src/libs/db.js
+++ src/libs/db.js
@@ -1,11 +1,23 @@
 import { createPool } from "mysql2/promise";
 
 export const conn = createPool({
-  database: "clicklunch",
-  username: process.env.DATABASE_USERNAME,
-  host: process.env.DATABASE_HOST,
-  password: process.env.DATABASE_PASSWORD,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  host: "localhost",
+  user: "root",
+  password: "n0m3l0",
+  port: 3308,
+  database: "clickluch",
 });
+
+// export const conn = createPool({
+//     host:'localhost',
+//     user:'root',
+//     password:'LuMITY_BV1',
+//     port:3306,
+//     database:'clickluch'
+// })
+
+// database: clicklunch
+// username: 2l3m81j21fxzbehsiz9t
+// host: aws.connect.psdb.cloud
+// password: pscale_pw_5iIktqxOb7x7Q0Ec419OzTMbRuCEuZnv7Bukc41hD1Q
+
