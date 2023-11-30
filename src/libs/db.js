c6955cfdf92e3c0ee91f72e100d1c3be8a935054
PilloSm <corzo.arguello.luis@gmail.com>
2023-11-30 02:37:22 -0600

    dasfasdfa

--- src/libs/db.js
+++ src/libs/db.js
@@ -1,11 +1,10 @@
 import { createPool } from "mysql2/promise";
 
 export const conn = createPool({
-  database: "clickluch",
-  user: 'root',
-  host: 'localhost',
-  password: 'LuMITY_BV1',
-  port: 3306,
+  database: "clicklunch",
+  user: 'unamusn7qg592mugxggs',
+  host: process.env.DATABASE_HOST,
+  password: 'pscale_pw_C179ZTwv9Yt6Tl6e1eDG68MbcrIH7nyTD5P7Is6OZLm',
   ssl: {
     rejectUnauthorized: false,
   },
