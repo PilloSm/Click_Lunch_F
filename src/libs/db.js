b174d7831563e8eee6e4f06930cc57823beb8a24
PilloSm <corzo.arguello.luis@gmail.com>
2023-11-30 00:09:40 -0600

    dfasdfada

--- src/libs/db.js
+++ src/libs/db.js
@@ -2,7 +2,7 @@ import { createPool } from "mysql2/promise";
 
 export const conn = createPool({
   database: 'clicklunch',
-  user: vo9zbl973a7jeoax56hs,
+  user: 'vo9zbl973a7jeoax56hs',
   host: 'aws.connect.psdb.cloud',
   password: 'pscale_pw_G7mXmMTKOmKxWsfU7IdVoiPJKBAaIwcqbU96wgrEKD8',
   ssl: {
