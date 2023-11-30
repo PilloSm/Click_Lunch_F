c0ec227e59baed71ca77cfbd1ce6b3c05793727e
PilloSm <corzo.arguello.luis@gmail.com>
2023-11-30 00:01:22 -0600

    r2d2

--- src/libs/db.js
+++ src/libs/db.js
@@ -1,10 +1,10 @@
 import { createPool } from "mysql2/promise";
 
 export const conn = createPool({
-  database: "clicklunch",
-  user: ezafa6bhhv1pma4j8zpp,
-  host: aws.connect.psdb.cloud,
-  password: pscale_pw_Mi4Y16Vu1rRrRcNSwEDZp9hHqjAltKE6oc9T5NLZIsa,
+  ddatabase: 'clicklunch',
+  user: 'vo9zbl973a7jeoax56hs',
+  host: 'aws.connect.psdb.cloud',
+  password: 'pscale_pw_G7mXmMTKOmKxWsfU7IdVoiPJKBAaIwcqbU96wgrEKD8',
   ssl: {
     rejectUnauthorized: false,
   },
