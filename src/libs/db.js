cadbb3e54e62e57096b1e86818041f553f0354fc
PilloSm <corzo.arguello.luis@gmail.com>
2023-11-30 00:14:10 -0600

    fadfa

--- src/libs/db.js
+++ src/libs/db.js
@@ -1,8 +1,11 @@
 import { createPool } from "mysql2/promise";
 
 export const conn = createPool({
-  database: 'clicklunch',
-  user: 'vo9zbl973a7jeoax56hs',
-  host: 'aws.connect.psdb.cloud',
-  password: 'pscale_pw_G7mXmMTKOmKxWsfU7IdVoiPJKBAaIwcqbU96wgrEKD8',
+  database: "clicklunch",
+  user: "vo9zbl973a7jeoax56hs",
+  host: "aws.connect.psdb.cloud",
+  password: "pscale_pw_G7mXmMTKOmKxWsfU7IdVoiPJKBAaIwcqbU96wgrEKD8",
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
